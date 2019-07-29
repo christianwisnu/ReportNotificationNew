2a5274f12e122eeabcad3298e09094c4dab6ba27
christianwisnu <c_vincentius@yahoo.com>
2019-07-29 09:19:14 +0700

    link php pindah

--- app/src/main/java/utilities/Link.java
+++ app/src/main/java/utilities/Link.java
@@ -4,8 +4,8 @@ import service.BaseApiService;
 
 public class Link {
 
-    public static final String BASE_URL_API = "http://softchrist.com/report_notification/php/";
-    public static final String BASE_URL_NOTIF = "http://softchrist.com/report_notification/php/notification.php";
+    public static final String BASE_URL_API = "http://149.28.156.39/report_notification/php/";
+    public static final String BASE_URL_NOTIF = "http://149.28.156.39/report_notification/php/notification.php";
 
     // Mendeklarasikan Interface BaseApiService
     public static BaseApiService getAPIService(){
